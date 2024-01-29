2b1b7ccd9fc2aee43d92e2e169c209ef0321233f
ishmeet09 <108580632+ishmeet09@users.noreply.github.com>
2024-01-29 18:22:11 +0530

    Add files via upload

---
+++ 232.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+class MyQueue {
+public:
+stack<int>s1,s2;
+//doing the method in which pop is in O(n) and push is in O(1)
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        int pop=-1;
+        if(!s2.empty()){
+            pop=s2.top();
+        }
+        else{
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            pop=s2.top();
+        }
+        s2.pop();
+        return pop;
+    }
+    
+    int peek() {
+        int front=-1;
+        if(!s2.empty()){
+            return s2.top();
+        }
+        else{
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            front=s2.top();
+        }
+      
+        return front;
+    }
+    
+    bool empty() {
+        return s1.empty()&&s2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+int main(){
+
+return 0;
+}
